cdc5926cb6e30e1ceb7179682c12c8fea4cad9a9
javaloayza <java.loayza@gmail.com>
2023-12-15 23:35:50 -0500

    first commit

---
+++ green/array-prototype-last.js
@@ -0,0 +1,20 @@
+// 2619. Array Prototype Last
+// Easy
+// Companies
+// Hint
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+// You may assume the array is the output of JSON.parse.
+
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function () {
+
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
