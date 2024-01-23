2bda9e6d655de3afa5e3bb24cae1316775439343
Aldo Javier Loayza <95065154+javaloayza@users.noreply.github.com>
2024-01-22 21:50:06 -0500

    Update

--- green/merge-sorted-array.js
+++ green/merge-sorted-array.js
@@ -3,7 +3,7 @@
  * @param {number} m
  * @param {number[]} nums2
  * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead. 1.
+ * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
     
