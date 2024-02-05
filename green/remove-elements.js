e2f09cf467cbc09f4c6c10b4ddbcf211487a3ec4
Aldo Javier Loayza <95065154+javaloayza@users.noreply.github.com>
2024-02-04 20:07:04 -0500

    Update remove-elements.js

--- green/remove-elements.js
+++ green/remove-elements.js
@@ -3,6 +3,17 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        while(true)
+        {
+            auto it = find(nums.begin(),nums.end(),val);
+            
+            if(it != nums.end())
+                nums.erase(it);
+            else break;
+        }
+        return nums.size();
+    }
 };
